a73f14439dc7dd35e0c472e80036c69b37d78739
Terrence Curran <tcurran@approachingpi.com>
2016-12-06 17:54:54 -0700

    started adding unit testing to preceed refactoring

---
+++ Code/JpegDeux-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  JpegDeux-Bridging-Header.h
+//  JPEGDeux
+//
+//  Created by Terrence Curran on 12/6/16.
+//
+//
+
+#ifndef JpegDeux_Bridging_Header_h
+#define JpegDeux_Bridging_Header_h
+
+#import "Classes/BackgroundImageView.h"
+#import "PetersTypes.h"
+
+#endif /* JpegDeux_Bridging_Header_h */
